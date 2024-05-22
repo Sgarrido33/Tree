afe0fbb90f03f071223cce7b913d52cd9aebbfc1
Sgarrido33 <103430365+Sgarrido33@users.noreply.github.com>
2024-05-22 00:25:35 -0500

    Update extree.h

--- extree.h
+++ extree.h
@@ -25,3 +25,37 @@ public:
         return result; 
     }
 };
+
+
+
+
+
+
+class Solution {
+public:
+    int minDiffInBST(TreeNode* root) {
+        int minDiff = INT_MAX;
+        TreeNode* prev = nullptr;
+        inorderTraversal(root, prev, minDiff);
+        return minDiff;
+    }
+
+private:
+    void inorderTraversal(TreeNode* node, TreeNode*& prev, int& minDiff) {
+        if (node == nullptr) {
+            return;
+        }
+
+
+        inorderTraversal(node->left, prev, minDiff);
+
+  
+        if (prev != nullptr) {
+            minDiff = min(minDiff, node->val - prev->val);
+        }
+        prev = node;
+
+
+        inorderTraversal(node->right, prev, minDiff);
+    }
+};
