9209a901b669d99ce39f2d8fd681fa77fa2f3fa8
Sgarrido33 <103430365+Sgarrido33@users.noreply.github.com>
2024-05-22 10:36:32 -0500

    Update extree.h

--- extree.h
+++ extree.h
@@ -59,3 +59,26 @@ private:
         inorderTraversal(node->right, prev, minDiff);
     }
 };
+
+
+
+
+
+
+
+class Solution {
+public:
+    bool isMirror(TreeNode* t1, TreeNode* t2) {
+    if (t1 == nullptr and t2 == nullptr) {
+        return true;
+    }
+    if (t1 == nullptr or t2 == nullptr) {
+        return false;
+    }
+    return (t1->val == t2->val) and isMirror(t1->left,t2->right) and isMirror(t1->right,t2->left);
+}
+
+    bool isSymmetric(TreeNode* root) {
+        return isMirror(root, root);
+    }
+};
